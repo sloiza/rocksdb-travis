ea0d572f3acc641544f249b883f01824ed63b3b7
sloiza <samiloiza@gmail.com>
2015-10-08 08:01:52 -0300

    Rocksdb source for testing travis

---
+++ ROCKS/util/build_version.cc
@@ -0,0 +1,4 @@
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:0be260523c5e1b995c221853f71394ee172bec17";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2015-10-01";
+const char* rocksdb_build_compile_date = __DATE__;
